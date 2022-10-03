e104590bf720eb1ee90f88a9448b5dcd38f944cb
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2022-10-03 14:32:36 +0500

    -opacity meny anim

--- src/js/components/menuAnimation.js
+++ src/js/components/menuAnimation.js
@@ -5,15 +5,15 @@ const burger = document.querySelector(".burger");
 const body = document.querySelector(".page__body");
 
 document.addEventListener("DOMContentLoaded", () => {
-	burger.addEventListener("click", () => {
-		if (!burger.classList.contains("burger--active")) {
-			gsap.fromTo(menu, { x: "100%", opacity: 0 }, { x: 0, opacity: 1 });
-			burger.classList.add("burger--active");
-			body.style.overflow = "hidden";
-		} else {
-			gsap.to(menu, { x: "-100%", opacity: 0 });
-			burger.classList.remove("burger--active");
-			body.style.overflow = "visible";
-		}
-	});
+    burger.addEventListener("click", () => {
+        if (!burger.classList.contains("burger--active")) {
+            gsap.fromTo(menu, { x: "100%", opacity: 0 }, { x: 0, opacity: 1 });
+            burger.classList.add("burger--active");
+            body.style.overflow = "hidden";
+        } else {
+            gsap.to(menu, { x: "-100%" });
+            burger.classList.remove("burger--active");
+            body.style.overflow = "visible";
+        }
+    });
 });
